61f89e0471a7a253ee3e3280a328ec629e448fd3
Honaker <chris98@vt.edu>
2019-03-03 19:05:08 -0500

    tak

--- client.py
+++ client.py
@@ -38,10 +38,10 @@ else:
 	size = int(sys.argv[6])
 	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 	while 1:
-	    s.connect((host,port))
-	    checkpoint = 1
-	    print("[" + str(datetime.datetime.now())  + "] [Checkpoint " + str(checkpoint).zfill(2) + "] Connection to " + str(host) + " on port " + str(port)) 
-	    checkpoint += 1
+		s.connect((host,port))
+		checkpoint = 1
+		print("[" + str(datetime.datetime.now())  + "] [Checkpoint " + str(checkpoint).zfill(2) + "] Connection to " + str(host) + " on port " + str(port)) 
+		checkpoint += 1
 		print("[" + str(datetime.datetime.now())  + "] [Checkpoint " + str(checkpoint).zfill(2) + "] Listening for QR codes from RPi Camera that contain questions")
 		checkpoint += 1
 		
